110bb73ce3f75ff02f1b1db877d348754651fab2
connor <39143971+connorwise@users.noreply.github.com>
2024-05-08 00:53:06 -0400

    Stable Dynamic Crosshair Centering
    Encountered bizarre errors, in where certain aspect ratios caused mouse tracking to freeze up. Stared at it for a while, tried moving some code around and it was fixed!
    
    Also changed crosshair color for contrast

--- NoirShooterFiles/Assets/Controls/Scripts/PlayerController.gd
+++ NoirShooterFiles/Assets/Controls/Scripts/PlayerController.gd
@@ -1,10 +1,10 @@
 extends CharacterBody3D
 
-@export var WalkSpeed = 1.5
-@export var RunSpeed = 2
-@export var JumpSpeed = 2
-@export var Gravity = 9.8
+@export var WALK_SPEED : float = 5
+@export var RUN_SPEED : float = 7
+@export var JUMP_SPEED : float = 3.5
 
+@export var MOUSE_SENSITIVITY : float = 0.5
 @export var TILT_LOWER_LIMIT := deg_to_rad(-90.0)
 @export var TILT_UPPER_LIMIT := deg_to_rad(90.0)
 @export var CAMERA_CONTROLLER : Camera3D
@@ -18,12 +18,11 @@ var _tilt_input : float
 var _player_rotation : Vector3
 var _camera_rotation : Vector3
 
-var target_velocity = Vector3.ZERO
+#var target_velocity = Vector3.ZERO
+
 
 
 func _update_camera(delta):
-	
-	
 	_mouse_rotation.x += _tilt_input * delta
 	_mouse_rotation.x = clamp(_mouse_rotation.x, TILT_LOWER_LIMIT, TILT_UPPER_LIMIT)
 	_mouse_rotation.y += _rotation_input * delta
@@ -36,55 +35,52 @@ func _update_camera(delta):
 	
 	global_transform.basis = Basis.from_euler(_player_rotation)
 	
+	
 	_rotation_input = 0.0
 	_tilt_input = 0.0
-	
 	
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	
 func _unhandled_input(event):
+	pass
+	
+func _input(event):
+	
+	#This handles the mouse movement and translates it to rotation
 	_mouse_input = event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED
 	if _mouse_input:
-		_rotation_input = -event.relative.x
-		_tilt_input = -event.relative.y
-		#print(Vector2(_rotation_input, _tilt_input))
+		_rotation_input = -event.relative.x * MOUSE_SENSITIVITY
+		_tilt_input = -event.relative.y * MOUSE_SENSITIVITY
 		
-func _input(event):
 	if event.is_action_pressed("exit"):
 		get_tree().quit()
 		
 func _physics_process(delta):
-	var direction = Vector3.ZERO
-	
+	#Add gravity to player
+	if not is_on_floor():
+		velocity.y -= (gravity * delta)
+	var move_speed : float
 
+	if Input.is_action_pressed("run"):
+		move_speed = RUN_SPEED
+	else:
+		move_speed = WALK_SPEED
+		
 	_update_camera(delta)
 	
-	if Input.is_action_pressed("move_forward"):
-		direction.z += 1#WalkSpeed * delta
-	if Input.is_action_pressed("move_back"):
-		direction.z -= 1#WalkSpeed * delta
-	if Input.is_action_pressed("move_left"):
-		direction.x += 1#WalkSpeed * delta
-	if Input.is_action_pressed("move_right"):
-		direction.x -= 1#WalkSpeed * delta
-	if Input.is_action_pressed("jump"):
-		direction.y += 1#JumpSpeed * delta
+	if Input.is_action_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_SPEED#JumpSpeed * delta
 		
-	if direction != Vector3.ZERO:
-		direction = direction.normalized()
-		#$Pivot.Basis = Basis.looking_at(direction)
-	
-	target_velocity.x = direction.x * WalkSpeed
-	target_velocity.z = direction.z * WalkSpeed
-	target_velocity.y = direction.y * JumpSpeed
-	
-	if not is_on_floor():
-		target_velocity.y -= (gravity * delta)
-		
-
-	velocity = target_velocity
-	print(gravity*delta)
+	#get movement vector & normalize
+	var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * move_speed
+		velocity.z = direction.z * move_speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, WALK_SPEED)
+		velocity.z = move_toward(velocity.z, 0, WALK_SPEED)
 	
 	move_and_slide()
 
